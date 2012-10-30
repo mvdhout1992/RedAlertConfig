9ce15638d53ac0f48cb05241cff1bbce1f38e6f5
Maikel vd H <ma1kelvdh@gmail.com>
2012-10-30 19:15:12 +0100

    check if required data files exist and fail early if not

--- Files.cs
+++ Files.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using Nyerguds.Ini;
+using System.IO;
+using System.Windows.Forms;
 
 namespace RedAlertConfig
 {
@@ -12,12 +14,29 @@ namespace RedAlertConfig
 
         public static void Init()
         {
-            // Check if our needed files exist and acquire a lock to them
-
             DDrawINI = new IniFile("ddraw.ini", false, BooleanMode.TRUE_FALSE, Encoding.Default);
             RedAlertINI = new IniFile("redalert.ini", false, BooleanMode.YES_NO, Encoding.Default);
-            // FileStream.Lock()
 
+            Check_Required_File("ddraw.dll");
+            Check_Required_File("expand.mix");
+            Check_Required_File("expand2.mix");
+            Check_Required_File("ForceAMrules.ini");
+            Check_Required_File("hires1.mix");
+            Check_Required_File("thipx32.dll");
+        }
+
+        public static void Check_Required_File(string file)
+        {
+            String path = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "ConfigToolFiles"
+                + Path.DirectorySeparatorChar + file;
+            if (!File.Exists(path))
+            {
+                MessageBox.Show(String.Format("File '{0}' can't be found in the ConfigToolFiles folder, " +
+                "this is a file neccessary for this application to run so please restore it.", file), 
+                    "Required data file not found");
+
+                Environment.Exit(0);
+            }
         }
     }
 
